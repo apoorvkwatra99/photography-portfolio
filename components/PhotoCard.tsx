0885b8e66fe8342768768f89b35f693681658aec
apoorv-kwatra <apoorvkwatra99@gmail.com>
2026-08-13 16:35:47 -0400

    fix type bug again x2

--- components/PhotoCard.tsx
+++ components/PhotoCard.tsx
@@ -10,7 +10,6 @@ export default function PhotoCard({ photo }: { photo: Photo }) {
       />
       <div className="absolute inset-0 bg-black/0 group-hover:bg-black/40 transition-colors duration-300" />
       <div className="absolute bottom-0 left-0 right-0 p-4 translate-y-full group-hover:translate-y-0 transition-transform duration-300">
-        <p className="text-white text-sm font-medium leading-tight">{photo.title}</p>
         <p className="text-white/60 text-xs mt-0.5">{photo.placeLabel}</p>
       </div>
     </article>
